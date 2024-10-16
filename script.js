72b1c5bbe3a61034eb14b39eb3e0d0496686fe40
Adesoji <omoseebiadesojiadsoji@gmail.com>
2024-10-16 09:26:13 -0400

    jsday3

--- script.js
+++ script.js
@@ -1,44 +1,21 @@
-let demo = document.getElementById("demo");
-function showDate() {
-  demo.innerHTML = new Date().getFullYear();
-  //   console.log(new Date().getFullYear());
-  window.prompt("How old are you?");
-  document.write("Good morning");
+let bio = document.getElementById("bio");
+let name = document.getElementById("Adesoji");
+let state = document.getElementById("state");
+let education = document.getElementById("education")
+let hobby = document.getElementById("hobby")
+let tech = document.getElementById("tech")
+let flexibility = document.getElementById("flexibility")
+let freelancer = document.getElementById("freelancer")
+let status = document.getElementById("status")
+let age = document.querySelector("age")
+function about() {
+  bio.innerHTML = "My little story";
+ state.innerHTML = " i am from Osun state"
+ education.innerHTML = " Oscotech Esa oke"
+ hobby.innerHTML = "Love to play football and ps3"
+ tech.innerHTML = "I love to code and still on it"
+ flexibility.innerHTML ="I love to adapt to anywhere i found myself"
+status.innerHTML = "I am single"
+age.innerHTML = "I am 900yrs old"
+
 }
-
-// var
-var weight = "12kg";
-console.log(weight);
-
-// let
-let name = "Ade";
-console.log(name);
-let num = "1";
-let number = 1;
-const isPresent = true;
-// const is used to declare a constant variable
-const age = 12;
-// const student = {key: value}
-const student = { name: "Ade", age: 12, school: "OIC", isPresentToday: true };
-const names = ["Ade", 12, true, "OIC"];
-console.log(names.length);
-console.log(names[0]);
-
-let a = 5;
-let b = "6";
-console.log(a + Number(b));
-
-// ways of displaying data
-// 1. console.log()
-// 2.window.alert()
-// 3. DOM document.getElementById()
-// 4. window.prompt()
-// 5. document.write('Good morning')
-
-// datatypes
-// 1.string
-// 2.Number
-// 3. Boolean
-// 4.object: divided into object and array
-// 5. Undefined
-
